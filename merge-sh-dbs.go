89ae25b61f07513c695bf4ab47eed02a89c92fd4
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-07-16 13:31:34 +0000

    Handle domains_organizations [wip]
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- merge-sh-dbs.go
+++ merge-sh-dbs.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"runtime/debug"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -30,6 +31,16 @@ type country struct {
 	alpha3 string
 }
 
+// domainOrg holds data for domains_organizations table
+type domainOrg struct {
+	id          int64
+	domain      string
+	isTopDomain int
+	orgID       int64
+	orgName     string // computed
+	orgIDMerged int64  // computed
+}
+
 // mergeDatabases merged dbs[0] and dbs[1] into dbs[2]
 func mergeDatabases(dbs []*sql.DB) error {
 	/* countries
@@ -41,26 +52,27 @@ func mergeDatabases(dbs []*sql.DB) error {
 	| alpha3 | varchar(3)   | NO   | UNI | NULL    |       |
 	+--------+--------------+------+-----+---------+-------+
 	*/
+	fmt.Printf("countries...\n")
 	mdb := dbs[2]
 	_, err := mdb.Exec("delete from countries")
 	fatalOnError(err)
-	var cm [3]map[string]country
+	var countryMap [3]map[string]country
 	for i := 0; i < 2; i++ {
 		rows, err := dbs[i].Query("select code, name, alpha3 from countries")
 		fatalOnError(err)
 		var c country
-		cm[i] = make(map[string]country)
+		countryMap[i] = make(map[string]country)
 		for rows.Next() {
 			fatalOnError(rows.Scan(&c.code, &c.name, &c.alpha3))
-			cm[i][c.code] = c
+			countryMap[i][c.code] = c
 		}
 		fatalOnError(rows.Err())
 		fatalOnError(rows.Close())
 	}
-	cm[2] = make(map[string]country)
-	for code, c := range cm[0] {
-		c2, ok := cm[1][code]
-		cm[2][code] = c
+	countryMap[2] = make(map[string]country)
+	for code, c := range countryMap[0] {
+		c2, ok := countryMap[1][code]
+		countryMap[2][code] = c
 		if !ok {
 			fmt.Printf("Country from 1st (%+v) missing in 2nd, adding\n", c)
 			continue
@@ -69,18 +81,18 @@ func mergeDatabases(dbs []*sql.DB) error {
 			fmt.Printf("Country from 1st (%+v) different in 2nd, using first\n", c)
 		}
 	}
-	for code, c := range cm[1] {
-		c1, ok := cm[0][code]
+	for code, c := range countryMap[1] {
+		c1, ok := countryMap[0][code]
 		if !ok {
 			fmt.Printf("Country from 2nd (%+v) missing in 1st, adding\n", c)
-			cm[2][code] = c
+			countryMap[2][code] = c
 			continue
 		}
 		if c.name != c1.name || c.alpha3 != c1.alpha3 {
 			fmt.Printf("Country from 2nd (%+v) different in 1st, using first\n", c)
 		}
 	}
-	for _, c := range cm[2] {
+	for _, c := range countryMap[2] {
 		_, err := mdb.Exec("insert into countries(code, name, alpha3) values(?, ?, ?)", c.code, c.name, c.alpha3)
 		fatalOnError(err)
 	}
@@ -92,10 +104,12 @@ func mergeDatabases(dbs []*sql.DB) error {
 	| name  | varchar(191) | NO   | UNI | NULL    |                |
 	+-------+--------------+------+-----+---------+----------------+
 	*/
+	fmt.Printf("organizations...\n")
 	_, err = mdb.Exec("delete from organizations")
+	fatalOnError(err)
 	var orgID2Str [3]map[int64]string
 	var orgStr2ID [3]map[string]int64
-	orgStr := make(map[string]struct{})
+	orgStr := make(map[string]string)
 	fatalOnError(err)
 	for i := 0; i < 2; i++ {
 		rows, err := dbs[i].Query("select id, name from organizations")
@@ -107,32 +121,80 @@ func mergeDatabases(dbs []*sql.DB) error {
 		for rows.Next() {
 			fatalOnError(rows.Scan(&id, &name))
 			orgID2Str[i][id] = name
-			orgStr2ID[i][name] = id
-			orgStr[name] = struct{}{}
+			orgStr2ID[i][strings.ToLower(name)] = id
+			orgStr[strings.ToLower(name)] = name
 		}
 		fatalOnError(rows.Err())
 		fatalOnError(rows.Close())
 	}
 	orgID2Str[2] = make(map[int64]string)
 	orgStr2ID[2] = make(map[string]int64)
-	for id, name := range orgID2Str[0] {
-		_, ok := orgID2Str[1][id]
+	for name, id := range orgStr2ID[0] {
+		_, ok := orgStr2ID[1][name]
 		if !ok {
 			fmt.Printf("Organization from 1st (id=%d, name=%s) missing in 2nd, adding\n", id, name)
 			continue
 		}
 	}
-	for id, name := range orgID2Str[1] {
-		_, ok := orgID2Str[0][id]
+	for name, id := range orgStr2ID[1] {
+		_, ok := orgStr2ID[0][name]
 		if !ok {
 			fmt.Printf("Organization from 2nd (id=%d, name=%s) missing in 1st, adding\n", id, name)
 			continue
 		}
 	}
-	for name := range orgStr {
+	for lName, name := range orgStr {
 		_, err := mdb.Exec("insert into organizations(name) values(?)", name)
 		fatalOnError(err)
+		rows, err := mdb.Query("select id from organizations where name = ?", name)
+		fatalOnError(err)
+		var id int64
+		for rows.Next() {
+			fatalOnError(rows.Scan(&id))
+		}
+		fatalOnError(rows.Err())
+		fatalOnError(rows.Close())
+		orgID2Str[2][id] = name
+		orgStr2ID[2][lName] = id
 	}
+	/* domains_organizations
+	+-----------------+--------------+------+-----+---------+----------------+
+	| Field           | Type         | Null | Key | Default | Extra          |
+	+-----------------+--------------+------+-----+---------+----------------+
+	| id              | int(11)      | NO   | PRI | NULL    | auto_increment |
+	| domain          | varchar(128) | NO   | UNI | NULL    |                |
+	| is_top_domain   | tinyint(1)   | YES  |     | NULL    |                |
+	| organization_id | int(11)      | NO   | MUL | NULL    |                |
+	+-----------------+--------------+------+-----+---------+----------------+
+	*/
+	fmt.Printf("domains_organizations...\n")
+	_, err = mdb.Exec("delete from domains_organizations")
+	fatalOnError(err)
+	var domainMap [3]map[int64]domainOrg
+	for i := 0; i < 2; i++ {
+		rows, err := dbs[i].Query("select id, domain, is_top_domain, organization_id from domains_organizations")
+		fatalOnError(err)
+		var do domainOrg
+		domainMap[i] = make(map[int64]domainOrg)
+		for rows.Next() {
+			fatalOnError(rows.Scan(&do.id, &do.domain, &do.isTopDomain, &do.orgID))
+			// Map into merged organization_id - must succeed
+			orgName, ok := orgID2Str[i][do.orgID]
+			if !ok {
+				fatalf("cannot map organization ID %d from #%d input database", do.orgID, i+1)
+			}
+			do.orgName = orgName
+			orgIDMerged, ok := orgStr2ID[i][strings.ToLower(do.orgName)]
+			if !ok {
+				fatalf("cannot map organization ID %d -> Name %s from #%d input database", do.orgID, do.orgName, i+1)
+			}
+			do.orgIDMerged = orgIDMerged
+			domainMap[i][do.id] = do
+		}
+		fatalOnError(rows.Err())
+		fatalOnError(rows.Close())
+	}
+	// domainMap[2] = make(map[int64]domainOrg)
 	return nil
 }
 
