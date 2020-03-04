49b638ef8d791cbec710fb240b53cbb0251ba85e
lukaszgryglicki <lukaszgryglicki@o2.pl>
2020-03-04 05:20:22 +0000

    Update structure
    Signed-off-by: lukaszgryglicki <lukaszgryglicki@o2.pl>

--- dump_struct.sql
+++ dump_struct.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.17  Distrib 10.4.6-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
 --
--- Host: dev-analytics-sortinghat-dev.ch92vofnmy4d.us-west-2.rds.amazonaws.com    Database: sortinghat
+-- Host: ac3e91d5853dc11ea9212068202d388e-1942592115.us-west-2.elb.amazonaws.com    Database: sortinghat
 -- ------------------------------------------------------
--- Server version	10.3.8-MariaDB-log
+-- Server version	5.5.5-10.3.16-MariaDB-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -46,9 +46,8 @@ CREATE TABLE `domains_organizations` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `_domain_unique` (`domain`),
   KEY `organization_id` (`organization_id`),
-  CONSTRAINT `domains_organizations_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `top_domain_check` CHECK (`is_top_domain` in (0,1))
-) ENGINE=InnoDB AUTO_INCREMENT=1817 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+  CONSTRAINT `domains_organizations_ibfk_1` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7344 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +68,24 @@ CREATE TABLE `enrollments` (
   KEY `organization_id` (`organization_id`),
   CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`uuid`) REFERENCES `uidentities` (`uuid`) ON DELETE CASCADE,
   CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`organization_id`) REFERENCES `organizations` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5347 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=74900 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `enrollments_archive`
+--
+
+DROP TABLE IF EXISTS `enrollments_archive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `enrollments_archive` (
+  `archived_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `id` int(11) NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `uuid` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `organization_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +107,25 @@ CREATE TABLE `identities` (
   UNIQUE KEY `_identity_unique` (`name`,`email`,`username`,`source`),
   KEY `uuid` (`uuid`),
   CONSTRAINT `identities_ibfk_1` FOREIGN KEY (`uuid`) REFERENCES `uidentities` (`uuid`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `identities_archive`
+--
+
+DROP TABLE IF EXISTS `identities_archive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `identities_archive` (
+  `archived_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `id` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `email` varchar(128) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `username` varchar(128) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `source` varchar(32) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `uuid` varchar(128) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `last_modified` datetime(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -119,7 +154,7 @@ CREATE TABLE `organizations` (
   `name` varchar(191) COLLATE utf8mb4_unicode_520_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `_name_unique` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=21725 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=36012 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,8 +175,26 @@ CREATE TABLE `profiles` (
   PRIMARY KEY (`uuid`),
   KEY `country_code` (`country_code`),
   CONSTRAINT `profiles_ibfk_1` FOREIGN KEY (`uuid`) REFERENCES `uidentities` (`uuid`) ON DELETE CASCADE,
-  CONSTRAINT `profiles_ibfk_2` FOREIGN KEY (`country_code`) REFERENCES `countries` (`code`) ON DELETE CASCADE,
-  CONSTRAINT `is_bot_check` CHECK (`is_bot` in (0,1))
+  CONSTRAINT `profiles_ibfk_2` FOREIGN KEY (`country_code`) REFERENCES `countries` (`code`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `profiles_archive`
+--
+
+DROP TABLE IF EXISTS `profiles_archive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profiles_archive` (
+  `archived_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `uuid` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `name` varchar(128) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `email` varchar(128) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `gender` varchar(32) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `gender_acc` int(11) DEFAULT NULL,
+  `is_bot` tinyint(1) DEFAULT NULL,
+  `country_code` varchar(2) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -158,6 +211,20 @@ CREATE TABLE `uidentities` (
   PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `uidentities_archive`
+--
+
+DROP TABLE IF EXISTS `uidentities_archive`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `uidentities_archive` (
+  `archived_at` datetime(6) NOT NULL DEFAULT current_timestamp(6),
+  `uuid` varchar(128) COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `last_modified` datetime(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -168,4 +235,4 @@ CREATE TABLE `uidentities` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-07-18  6:06:46
+-- Dump completed on 2020-03-04  5:16:40
